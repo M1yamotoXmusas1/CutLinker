f1bc3685869364f97294c4ec0bc982681730d02a
MiyamotoMusashi <MiyamotoMusashi>
2025-11-04 22:20:49 +0300

    добавлена дб

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 // 1. Получить запрос
@@ -11,6 +15,7 @@ import (
 // 4. занести в базу данных
 // 5. создать страничку
 // 6. profit
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	str := "Hello World!"
 	b := []byte(str)
@@ -22,9 +27,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Корректно обработан HTTP запрос!")
 	}
 }
+
 func main() {
-	fmt.Println("Start")
-	http.HandleFunc("/default", handler)
+	connStr := "postgres://postgres:AZRAELBEATS@localhost:5431/golinkdb?sslmode=disable"
+
+	db, errdb := sql.Open("postgres", connStr)
+
+	if errdb != nil {
+		log.Fatal(errdb)
+	}
+	if errdb = db.Ping(); errdb != nil {
+		log.Fatal("Ошибка Open db:", errdb)
+	}
+	fmt.Println("Дб запустилась!")
+	defer db.Close()
+
+	http.HandleFunc("/createlink", handler)
+	http.HandleFunc("/redirect", handler)
 
 	err := http.ListenAndServe(":10000", nil)
 	if err != nil {
