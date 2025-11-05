0a21da4f29ceff1f059721ab7ff1d8c71ff0a7d7
eqstAzrael <bazirashvili@gmail.com>
2025-11-05 17:55:21 +0300

    отправка ссылки на сервак

--- front/src/link/Link.jsx
+++ front/src/link/Link.jsx
@@ -1,14 +1,40 @@
 import styles from './Link.module.css'
+import { useState } from 'react';
 
 function Link() {
+
+    const [link, setLink] = useState("");
+
+    function linkChange(e) {
+        setLink(e.target.value);
+    }
     
+    async function sendLink() {
+
+        const response = await fetch('#URL', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ link })
+            });
+  
+        if (response.ok) {
+            alert('Ссылка отправлена в Go!');
+        }
+    }   
+
     return(
         <div className={styles.block}>
-            <input className={styles.link} value="Вставьте вашу ссылку"/>
-            <button className={styles.button}> Сократить </button>
+            <input 
+                className={styles.link}  
+                onChange={linkChange}
+                placeholder="Введите текст..."
+            />
+            <button className={styles.button} onClick={sendLink}> Сократить </button>
         </div>
     )   
 
 }
+
+
 
 export default Link
